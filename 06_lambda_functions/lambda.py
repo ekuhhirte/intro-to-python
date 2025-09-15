223dbedc02e0fe73c9006eaddd01c12a168bfa2e
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-09-15 17:46:58 +0000

    weird error

--- 06_lambda_functions/lambda.py
+++ 06_lambda_functions/lambda.py
@@ -1,11 +1,16 @@
 # Create a lambda function
 
 add = lambda x, y: x + y
+#implement subtractiojn, multiplicaiton, and division
+#check the division by 0
 
+divide = lambda x, y: x / y if y!= 0 else "Error: Division by zero!"
 
 #test the function
 #create a list of tuples
 inputs = [(10,5), (8,0), (14, 6)]
 
 for x, y in inputs:
-    return 0
+    print(f"Inputs: {x}, {y}")
+    print(f"Addiction: {add(x,y)}")
+    print(f"Division: {divide(x,y)}\n")
