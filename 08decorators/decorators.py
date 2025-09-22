799008374e87a6593da92852754ff47b0515ef8a
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-09-22 17:23:15 +0000

    New directory

---
+++ 08decorators/decorators.py
@@ -0,0 +1,7 @@
+#measure the execution time of a funciton
+
+#import time functions (Specific import)
+from time import sleep
+
+def f():
+    sleep(0.3) # stops the execution for the given number of seconds
