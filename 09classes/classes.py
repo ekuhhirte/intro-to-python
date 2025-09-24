cc297da9affbf323e10d7d285a60259504d1850b
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-09-24 17:36:27 +0000

    more in class work with classes

--- 09classes/classes.py
+++ 09classes/classes.py
@@ -1,3 +1,11 @@
+
+
+
+
+
+#example in class work for a very simple class
+
+'''
 class Point:
     x = 10
     y = 7
@@ -6,3 +14,13 @@ pointer = Point()
 
 print(pointer.x)
 print(pointer.y)
+
+print(Point.x)
+
+print()
+pointer.x = 34
+print(Point.x)
+
+del pointer.x
+print()
+'''
