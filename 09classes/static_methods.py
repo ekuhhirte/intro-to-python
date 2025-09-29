79a8efaf8dea179e37e529910425b936a2bdbf1c
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-09-29 17:49:03 +0000

    last of the in class work for today

--- 09classes/static_methods.py
+++ 09classes/static_methods.py
@@ -4,8 +4,14 @@ class PathUtils:
     @staticmethod
     def get_extension(filename):
         """Return the file extension (including .)"""
-        
-        pass
+        return filename[filename.rfind("."):] if "." in filename else ""
+    
+    ### TODO
+    # static get_directory - extracts directory path
+
+    # static get_basename - extracts the file or directory name
 
 # Usage
 print(PathUtils.get_extension("1.txt"))
+print(PathUtils.get_extension("image.img"))
+print(PathUtils.get_extension("file"))
