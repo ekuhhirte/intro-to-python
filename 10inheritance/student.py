211ceaf9aefb59e5cfd4dd6803401a7ca806b03b
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-10-06 17:29:36 +0000

    more in class work being saved

--- 10inheritance/student.py
+++ 10inheritance/student.py
@@ -1,5 +1,25 @@
 class Student:
-    def __init__(self, name = "", student_id = 0, major = ""):
+    def __init__(self, name = "", student_id = "", major = ""):
         self.name = name
         self.student_id = student_id
         self.major = major
+    
+    def __str__(self):
+        return f"Name: {self.name}, ID: {self.student_id}, Major: {self.major}"
+    
+class UndergraduateStudent(Student):
+    # Overriding of methods
+    def __init__(self, name, student_id, major, year = ""):
+        super().__init__(name, student_id, major)
+        self.year = year
+
+    def __str__(self):
+        return super().__str__() + f", Year: {self.year}"
+
+
+if __name__ == "__main__":
+    student = Student("Jack", "U1010", "Computer Science")
+    print(student)
+
+    undergrad = UndergraduateStudent("Alice", "R4564", "Physics", "Freshman")
+    print(undergrad)
