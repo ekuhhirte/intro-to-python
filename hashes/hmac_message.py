ce403cee606182e80ca6302d4f8d076e690f5a19
ekuhhirte <ekuhhirte0836@floridapoly.edu>
2025-10-27 17:46:34 +0000

    hopefully the last of the in class work

--- hashes/hmac_message.py
+++ hashes/hmac_message.py
@@ -1,2 +1,16 @@
 import hmac # hash-based message authentificaiton code
+import hashlib
 
+def calculate_hash(key: str, message:str ):
+    b_key = key.encode()
+    b_message = message.encode()
+    hash_value = hmac.new(b_key, b_message, hashlib.sha256) #generates hash based message authentification code, requires the message, key, and hash method
+    return hash_value.hexdigest()
+
+key = "123456789"
+hash1 = calculate_hash(key, "Pay me $100")
+hash2 = calculate_hash(key, "Pay me $1000")
+hash3 = calculate_hash(key, "Pay me $100")
+
+print(hash1 == hash2)
+print(hash1 == hash3)
